c263a189905f83afd0973e5679575631ad848435
franciscocesar63 <franciscocesar888@gmail.com>
2021-02-25 23:41:19 -0300

    Criado Repository e Service para a Categoria

---
+++ src/main/java/com/fcocesar/cursospring/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.fcocesar.cursospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fcocesar.cursospring.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+		
+	
+}
